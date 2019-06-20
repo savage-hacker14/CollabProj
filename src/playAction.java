c5056dab37d084aa42843103593e05456075f5b9
PieDude2612 <arjun.viswanathan2612@gmail.com>
2019-06-20 17:14:37 -0400

    Partial GUI, buttons not aligned.

---
+++ src/playAction.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+/**
+ * The class for when the play button on the mainFrame is clicked. 
+ * @author arjun
+ * Date: 6/20/19
+ */
+public class playAction {
+
+	public playAction() {} //default constructor, no code required
+	
+	public static void start() {
+		//the code goes here.
+	}
+
+}
