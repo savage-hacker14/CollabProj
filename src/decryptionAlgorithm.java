37f43273b11272902aed30223492699a02512a9d
PieDude2612 <arjun.viswanathan2612@gmail.com>
2019-06-21 19:02:05 -0400

    Partial algorithm mark 2. Logic fixes needed...

--- src/decryptionAlgorithm.java
+++ src/decryptionAlgorithm.java
@@ -22,8 +22,10 @@ public class decryptionAlgorithm {	//TODO - finish up the special character logi
 		keypadNumber = input.nextInt();
 		
 		while((specialCases.equalsIgnoreCase("N")) && (keypadNumber >10)) {
+			
 			System.out.print("Incorrect Numbers! Try Again: ");
 			keypadNumber = input.nextInt();
+			
 		}
 		
 		System.out.print("Enter Max Capacity: ");
@@ -32,8 +34,11 @@ public class decryptionAlgorithm {	//TODO - finish up the special character logi
 		outcomes = solveAllOutcomes(keypadNumber, maxCap, specialCases);
 		
 		for(int count = 0; count < outcomes.size(); count++) {
+			
 			System.out.println(Integer.valueOf(outcomes.get(count)));
+			
 		}
+		
 	}
 	
 	/**
@@ -50,37 +55,27 @@ public class decryptionAlgorithm {	//TODO - finish up the special character logi
 		ArrayList<String> answers = new ArrayList<String>();	//creates arraylist holding answers
 		int[] numberChoices = new int[keyNums]; //dummy array just for the traversal process to put numbers in ArrayList
 		String number;
-		int index = 0;
 		
-		for(int i = 0; i < numberChoices.length; i++) {
+		for(int i = 0; i < numberChoices.length; i++) { //TODO - CHANGE THIS PLEB LOOP
+			
 			if(specials.equalsIgnoreCase("N")) {
+				
 				number = "";
 				number += generateRandomCombo(number, keyNums, max);	//generate random number combo
-							
-				int numberCon = Integer.valueOf(number);
 				
-				while(index < answers.size()) {
-					if(numberCon == (Integer.valueOf(answers.get(index)))){
-						
-						//traverses through the array list to see if there are any repeats. If there are, then it redoes the 
-						//random generation and begins from the top again
-						
-						number = "";
-						number += generateRandomCombo(number, keyNums, max);
-						numberCon = Integer.valueOf(number);
-						index = 0;
-					} else {
-						index++;	//if no repeats, proceed.
-					}
-				}
+				answers.add(checkRepeats(number, keyNums, max, answers));
 				
-				answers.add(number);	//add number to array list
 			} else if(specials.equalsIgnoreCase("Y")) {
+				
 				number = "";
 				int termUse = generator.nextInt(2); //pick at random to use either '*', '#', or both
+				
 			}
+			
 		}
+		
 		return answers;
+		
 	}
 	
 	/**
@@ -94,11 +89,54 @@ public class decryptionAlgorithm {	//TODO - finish up the special character logi
 	public static String generateRandomCombo(String num, int keys, int maximum) {
 		Random generator = new Random();
 		
-		for(int r = 0; r < maximum; r++) {
-			num += String.valueOf(generator.nextInt(keys));
+		int maxx = (int)Math.pow(10, maximum);
+		String rand = String.valueOf(generator.nextInt(maxx));
+		
+		while(rand.length() < maximum) {
+			rand = String.valueOf(generator.nextInt(maxx));
 		}
 		
+		num += rand; //from 0 to the greatest number possible from given code set.
+		
 		return num;
+		
+	}
+
+	/**
+	 * Checks if the randomly generated number is repeated anywhere in the array list. If it is, it regenerates a new 
+	 * number until the number generated is never repeated in the array list.
+	 * 
+	 * @param theNum, the Choices, theMax, theAnswers
+	 * 
+	 * pre: The array list must be defined and initialized
+	 * post: returns a newly generated random number within the bounds specified
+	 */
+	public static String checkRepeats(String theNum, int theChoices, int theMax, ArrayList<String> theAnswers) {
+		int index = 0;
+		int numberCon = Integer.valueOf(theNum);
+		
+		while(index < theAnswers.size()) {
+			
+			if(numberCon == (Integer.valueOf(theAnswers.get(index)))){
+				
+				//traverses through the array list to see if there are any repeats. If there are, then it redoes the 
+				//random generation and begins from the top again
+				
+				theNum = "";
+				theNum += generateRandomCombo(theNum, theChoices, theMax);
+				numberCon = Integer.valueOf(theNum);
+				index = 0;
+				
+			} else {
+				
+				index++;	//if no repeats, proceed.
+				
+			}
+			
+		}
+		
+		return theNum;
+		
 	}
 
 }
