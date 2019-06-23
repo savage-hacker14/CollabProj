90e7181d596a514578c680c2f7d5aa5cf7d641fc
PieDude2612 <arjun.viswanathan2612@gmail.com>
2019-06-23 09:54:21 -0400

    We have liftoff for number combinations with no special chars

--- src/decryptionAlgorithm.java
+++ src/decryptionAlgorithm.java
@@ -39,8 +39,9 @@ public class decryptionAlgorithm {	//TODO - finish up the special character logi
 			
 		}
 		
-		// Run time goes up expontentially
+		// Run time goes up exponentially
 		// 9 givens and 35 max capacity takes over 2 min to run
+		// Thankfully, this algorithm is only meant to run upto 8 characters. 
 		
 	}
 	
@@ -58,8 +59,12 @@ public class decryptionAlgorithm {	//TODO - finish up the special character logi
 		ArrayList<String> answers = new ArrayList<String>();	//creates arraylist holding answers
 		int[] numberChoices = new int[keyNums]; //dummy array just for the traversal process to put numbers in ArrayList
 		String number;
+		int total;
 		
-		for(int i = 0; i < numberChoices.length; i++) { //TODO - CHANGE THIS PLEB LOOP
+		int maxPoss = (factorial(keyNums)) / ((factorial(keyNums - max)) * (factorial(max)));
+		// the maxPoss for a given set of numbers is the factorial of the numbers, thinking mathematically. 
+		
+		for(total = 0; total < maxPoss; total++) { //TODO - CHANGE THIS PLEB LOOP 
 			
 			if(specials.equalsIgnoreCase("N")) {
 				
@@ -82,6 +87,28 @@ public class decryptionAlgorithm {	//TODO - finish up the special character logi
 	}
 	
 	/**
+	 * Recursively computes the value of the factorial for the number passed
+	 * @param theNumber
+	 * 
+	 * pre: the number passed must be initialized from main or some other number must be passed. 
+	 * post: returns its factorial
+	 */
+	public static int factorial(int theNumber) {
+		if(theNumber == 0) {
+			
+			return 1; //if there are no more numbers to multiply, then it returns 1 to make a non zero answer
+			
+		} else {
+			
+			return theNumber * factorial(theNumber - 1);
+			//calls the method again recursively to find the factorial.
+			
+		}
+		
+	}
+	
+	
+	/**
 	 * Generates the random number combination. 
 	 * 
 	 * @param num, keys, maximum
@@ -96,11 +123,14 @@ public class decryptionAlgorithm {	//TODO - finish up the special character logi
 		String rand = String.valueOf(generator.nextInt(maxx));
 		
 		while(rand.length() < maximum) {
+			
 			rand = String.valueOf(generator.nextInt(maxx));
+			
+			//As long as the number generated has lesser digits than the max digits given, 
+			//the loop keeps going. 
 		}
 		
-		num += rand; //from 0 to the greatest number possible from given code set.
-		
+		num += rand;		
 		return num;
 		
 	}
